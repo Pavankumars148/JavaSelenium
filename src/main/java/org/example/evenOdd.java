7ef5a38f4717a382c154d3b6010b4bd63f18587a
Pavan <pavankumar.148.003@gmail.com>
2024-08-03 01:08:39 +0530

    Your commit message

---
+++ src/main/java/org/example/evenOdd.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class evenOdd {
+
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        try {
+            Scanner sc= new Scanner(System.in);
+            int i= sc.nextInt();
+            checkEvenOrOdd(i);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void checkEvenOrOdd(int i) {
+        try {
+            if (i == 0||i<0) {
+                System.out.println("Given number " + i + " is not even or odd");
+            } else {
+                if (i % 2 == 0) {
+                    System.out.println("Given number " + i + " even");
+                } else {
+                    System.out.println("Given number " + i + " odd");
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
